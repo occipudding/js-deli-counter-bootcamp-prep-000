63a318c8c00979353cc5158bf0572abdfb7a591b
Joshua Suskin <jsuskin85@gmail.com>
2018-06-12 04:51:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var katzDeli = [];
+var katzDeliLine = [];
+var newName = [];
+
+function takeANumber(katzDeliLine, newName) {
+  
+  katzDeliLine.push(newName);
+  
+  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine,newName) {
+  
+  if(!katzDeliLine.length) {
+    return `There is nobody waiting to be served!`
+  } else {
+    return 
+  }
+  
+}
